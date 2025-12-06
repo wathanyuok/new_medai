7675baa3f23094064d197d953d45eb0c233f14e0
wathanyuok <wathanyuok@gmail.com>
2025-12-06 22:52:03 +0700

    re_popup1

--- next.config.ts
+++ next.config.ts
@@ -1,33 +1,3 @@
-// import type { NextConfig } from "next";
-
-// const nextConfig: NextConfig = {
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   webpack(config) {
-//     config.module.rules.push({
-//       test: /\.svg$/,
-//       use: ["@svgr/webpack"],
-//     });
-//     return config;
-//   },
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: 'https',
-//         hostname: 'cdn.komosite.com',
-//         port: '',
-//         pathname: '/**',
-//         search: '',
-//       },
-//     ],
-//   },
-// };
-
-
-// export default nextConfig;
-
-
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
